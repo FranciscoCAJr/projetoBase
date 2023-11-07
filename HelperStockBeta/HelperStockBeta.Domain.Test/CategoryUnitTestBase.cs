4795778e268114e88d15415c871d9e100a3c0819
Francisco <54285612+FranciscoCAJr@users.noreply.github.com>
2023-11-06 22:07:11 -0300

    Avaliacao 0311 testes unitarios

--- HelperStockBeta/HelperStockBeta.Domain.Test/CategoryUnitTestBase.cs
+++ HelperStockBeta/HelperStockBeta.Domain.Test/CategoryUnitTestBase.cs
@@ -11,22 +11,6 @@ namespace HelperStockBeta.Domain.Test
     #region Casos de testes positivos
     public class CategoryUnitTestBase
     {
-        [Fact(DisplayName ="Category name is not null")]
-        public void CreateCategory_WithValidParemeters_ResultValid()
-        {
-            Action action = () => new Category(1, "Categoria Teste");
-            action.Should()
-                .NotThrow<HelperStockBeta.Domain.Validation.DomainExceptionValidation>();
-        }
-
-        [Fact(DisplayName = "Category no present id parameter.")]
-        public void CreateCategory_IdParameterLess_ResultValid()
-        {
-            Action action = () => new Category("Categoria Teste");
-            action.Should()
-                .NotThrow<HelperStockBeta.Domain.Validation.DomainExceptionValidation>();
-        }
-        #endregion
         [Fact(DisplayName="Category name not null")]
         public void CreateProduct_withParameters_ResultValid()
         {
@@ -40,32 +24,8 @@ namespace HelperStockBeta.Domain.Test
             action.Should().NotThrow<HelperStockBeta.Domain.Validation.DomainExceptionValidation>();
         }
         #endregion
+
         #region Casos de testes negativos
-        [Fact(DisplayName = "Id negative exception.")]
-        public void CreateCategory_NegativeParameterId_ResultException()
-        {
-            Action action = () => new Category(-1, "Categoria Teste");
-            action.Should()
-                .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
-                .WithMessage("Identification is positive values!");
-        }
-        [Fact(DisplayName = "Name in Category null.")]
-        public void CreateCategory_NameParameterNull_ResultException()
-        {
-            Action action = () => new Category(1, null);
-            action.Should()
-                .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
-                .WithMessage("Invalid name. Name is required!");
-        }
-        [Fact(DisplayName = "Name short for Category null.")]
-        public void CreateCategory_NameParameterShort_ResultException()
-        {
-            Action action = () => new Category(1, "Ca");
-            action.Should()
-                .Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>()
-                .WithMessage("Name is minimum 3 charecters");
-        }
-        #endregion
         [Fact(DisplayName = "id negative exception")]
         public void CreateProduct_withParameters_ResultValid()
         {
@@ -84,5 +44,6 @@ namespace HelperStockBeta.Domain.Test
             Action action = () => new Product(1, "ca");
             action.Should().Throw<HelperStockBeta.Domain.Validation.DomainExceptionValidation>().WithMessage("invalid name, name needs at least 3 characters");
         }
+        #endregion
     }
 }
